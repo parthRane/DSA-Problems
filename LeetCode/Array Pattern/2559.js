65bbf6421d61efc283fa85f2031c6125eb39c0b6
Kalyan Bhowmick <kbhowmick067@gmail.com>
2025-01-03 10:55:29 +0530

    Added in same file

--- LeetCode/Array Pattern/2559.js
+++ LeetCode/Array Pattern/2559.js
@@ -10,22 +10,55 @@ Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
  * @param {number[][]} queries
  * @return {number[]}
  */
-var vowelStrings = function(words, queries) {
-    let vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
-    let ans = new Array(queries.length).fill(0);
-    let prefSum = new Array(words.length + 1).fill(0);
-    for(let i = 0;i<words.length;i++){
-        let firstChar = words[i].charAt(0);
-        let lastChar = words[i].at(-1); //words[i].charAt(words[i].length - 1);
-        if (vowels.includes(firstChar) && vowels.includes(lastChar)) {
-            prefSum[i + 1] = prefSum[i] + 1;
-        } else {
-            prefSum[i + 1] = prefSum[i];
-        }
+var vowelStrings = function (words, queries) {
+  let vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+  let ans = new Array(queries.length).fill(0);
+  let prefSum = new Array(words.length + 1).fill(0);
+  for (let i = 0; i < words.length; i++) {
+    let firstChar = words[i].charAt(0);
+    let lastChar = words[i].at(-1); //words[i].charAt(words[i].length - 1);
+    if (vowels.includes(firstChar) && vowels.includes(lastChar)) {
+      prefSum[i + 1] = prefSum[i] + 1;
+    } else {
+      prefSum[i + 1] = prefSum[i];
     }
-    for (let j = 0; j < queries.length; j++) {
-        let [start, end] = queries[j];
-        ans[j] = prefSum[end + 1] - prefSum[start];
+  }
+  for (let j = 0; j < queries.length; j++) {
+    let [start, end] = queries[j];
+    ans[j] = prefSum[end + 1] - prefSum[start];
+  }
+  return ans;
+};
+
+// Kalyan
+
+/**
+ * @param {string[]} words
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var vowelStrings = function (words, queries) {
+  let ans = [];
+  let prefixSum = new Array(words.length + 1).fill(0);
+  let vowels = new Set(["a", "e", "i", "o", "u"]);
+  let count = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (
+      vowels.has(words[i].charAt(0)) &&
+      vowels.has(words[i].charAt(words[i].length - 1))
+    ) {
+      count++;
+      prefixSum[i + 1] = count;
+    } else {
+      prefixSum[i + 1] = count;
     }
-    return ans;
+  }
+
+  for (let j = 0; j < queries.length; j++) {
+    let wordsCount = prefixSum[queries[j][1] + 1] - prefixSum[queries[j][0]];
+    ans.push(wordsCount);
+  }
+
+  return ans;
 };
